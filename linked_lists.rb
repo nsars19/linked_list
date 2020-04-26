ad9212e224d7653e6cae5ddb09d47a02f240c20a
Nick <nsardonini@gmail.com>
2020-04-26 14:11:07 -0400

    add instance methods to LinkedList

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,4 +1,46 @@
 class LinkedList
+  def initialize list = []
+    @list = list
+  end
+  def append value
+    @list.push value
+  end
+
+  def prepend value
+    @list.unshift value
+  end
+
+  def size
+    @list.size
+  end
+
+  def head
+    @list.first
+  end
+
+  def tail
+    @list.last
+  end
+
+  def at index
+    @list[index]
+  end
+
+  def pop
+    @list.pop
+  end
+
+  def contains? value
+    @list.include? value
+  end
+
+  def find value
+    @list.index value
+  end
+
+  def to_s
+    @list.each { |node| print "( #{node} ) -> " }; p nil
+  end
 end
 
 class Node
