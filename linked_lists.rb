a57a37c12502caaa873298fcecf5b94af9df4a21
Nick <nsardonini@gmail.com>
2020-04-26 19:06:41 -0400

    move Node class to separate file

--- linked_lists.rb
+++ linked_lists.rb
@@ -80,12 +80,3 @@ class LinkedList
     node.next_node = node.next_node.next_node
   end
 end
-
-class Node
-  attr_accessor :value, :next_node
-  
-  def initialize value = nil, next_node = nil
-    @value     = value
-    @next_node = next_node
-  end
-end
